c8c9955b896bfffd4ba07e6abfd09b0b4e30a96e
Namjun Kim <namjunemy@gmail.com>
2018-11-01 11:03:25 +0900

    init: Spring boot initialize settings

---
+++ src/main/java/io/namjune/springbootconceptandutilization/SpringBootConceptAndUtilizationApplication.java
@@ -0,0 +1,12 @@
+package io.namjune.springbootconceptandutilization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConceptAndUtilizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConceptAndUtilizationApplication.class, args);
+    }
+}
