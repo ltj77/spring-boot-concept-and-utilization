8fa0a1cd7e0b569a5aa1e9613c8a9c03564d72f5
Namjun Kim <namjunemy@gmail.com>
2018-12-04 13:07:52 +0900

    외부설정 - 타입-세이프 프로퍼티 @ConfigurationProperties 사용하기

---
+++ src/main/java/io/namjune/springbootconceptandutilization/SampleRunner.java
@@ -0,0 +1,22 @@
+package io.namjune.springbootconceptandutilization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleRunner implements ApplicationRunner {
+
+    @Autowired
+    NamjuneProperties namjuneProperties;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("==========================");
+        System.out.println(namjuneProperties.getName());
+        System.out.println(namjuneProperties.getAge());
+        System.out.println(namjuneProperties.getFullName());
+        System.out.println("==========================");
+    }
+}
--- src/main/java/io/namjune/springbootconceptandutilization/SpringBootConceptAndUtilizationApplication.java
+++ src/main/java/io/namjune/springbootconceptandutilization/SpringBootConceptAndUtilizationApplication.java
@@ -1,16 +1,14 @@
 package io.namjune.springbootconceptandutilization;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableConfigurationProperties(NamjuneProperties.class)
 public class SpringBootConceptAndUtilizationApplication {
 
     @GetMapping("/hello")
@@ -20,18 +18,5 @@ public class SpringBootConceptAndUtilizationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConceptAndUtilizationApplication.class, args);
-    }
-
-    @Bean
-    public ServletWebServerFactory serverFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8080);
-        return connector;
     }
 }
