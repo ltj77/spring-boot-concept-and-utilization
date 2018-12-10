99a3bf23c989dd4c293473e54e71c492258a9543
Namjun Kim <namjunemy@gmail.com>
2018-12-10 18:23:32 +0900

    spring5 WebTestClient를 활용해서 테스트하기

--- src/test/java/io/namjune/springbootconceptandutilization/sample/SampleControllerTest.java
+++ src/test/java/io/namjune/springbootconceptandutilization/sample/SampleControllerTest.java
@@ -1,6 +1,5 @@
 package io.namjune.springbootconceptandutilization.sample;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
@@ -9,15 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SampleControllerTest {
 
     @Autowired
-    TestRestTemplate testRestTemplate;
+    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
@@ -26,7 +25,9 @@ public class SampleControllerTest {
     public void hello() {
         when(mockSampleService.getName()).thenReturn("kim");
 
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello kim");
+        webTestClient.get().uri("/hello").exchange()
+            .expectStatus().isOk()
+            .expectBody(String.class).isEqualTo("hello kim");
+
     }
 }
