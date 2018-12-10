05da2843fd7c07671ff73eb1d7e98a8bd13a1afd
Namjun Kim <namjunemy@gmail.com>
2018-12-10 16:35:47 +0900

    기본 로깅 설정

--- src/main/java/io/namjune/springbootconceptandutilization/SampleRunner.java
+++ src/main/java/io/namjune/springbootconceptandutilization/SampleRunner.java
@@ -1,5 +1,7 @@
 package io.namjune.springbootconceptandutilization;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(SampleRunner.class);
 
     @Autowired
     private String hello;
@@ -16,9 +20,10 @@ public class SampleRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("==========================");
-        System.out.println(hello);
-        System.out.println("name: " + namjuneProperties.getName());
-        System.out.println("==========================");
+
+        logger.debug("==========================");
+        logger.debug(hello);
+        logger.debug("name: " + namjuneProperties.getName());
+        logger.debug("==========================");
     }
 }
