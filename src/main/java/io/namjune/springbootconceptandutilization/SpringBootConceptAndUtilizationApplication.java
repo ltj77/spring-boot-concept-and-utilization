f051482f41506c88cb64fada0ff7b975fef0c099
Namjun Kim <namjunemy@gmail.com>
2018-12-10 17:50:45 +0900

    Test환경이 MOCK일 때, MockMvc를 활용해서 테스트 하는 방법

--- src/main/java/io/namjune/springbootconceptandutilization/SpringBootConceptAndUtilizationApplication.java
+++ src/main/java/io/namjune/springbootconceptandutilization/SpringBootConceptAndUtilizationApplication.java
@@ -2,19 +2,9 @@ package io.namjune.springbootconceptandutilization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@EnableConfigurationProperties(NamjuneProperties.class)
 public class SpringBootConceptAndUtilizationApplication {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello Spring Boot";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConceptAndUtilizationApplication.class, args);
---
+++ src/main/java/io/namjune/springbootconceptandutilization/sample/SampleService.java
@@ -0,0 +1,11 @@
+package io.namjune.springbootconceptandutilization.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "namjune";
+    }
+}
