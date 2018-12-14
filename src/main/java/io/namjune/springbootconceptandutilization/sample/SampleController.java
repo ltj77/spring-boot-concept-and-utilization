6f821904b4b88d8a2f4e8ac308e6ddf4b8230af5
Namjun Kim <namjunemy@gmail.com>
2018-12-14 14:56:37 +0900

    @Rule, OutputCapture로 콘솔 로그 테스트

--- src/main/java/io/namjune/springbootconceptandutilization/sample/SampleController.java
+++ src/main/java/io/namjune/springbootconceptandutilization/sample/SampleController.java
@@ -1,6 +1,8 @@
 package io.namjune.springbootconceptandutilization.sample;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SampleController {
 
+    Logger logger = LoggerFactory.getLogger(SampleController.class);
+
     private final SampleService sampleService;
 
     @GetMapping("/hello")
     public String hello() {
+        logger.info("hello logger");
+        System.out.println("hello sout");
         return "hello " + sampleService.getName();
-
     }
 }
