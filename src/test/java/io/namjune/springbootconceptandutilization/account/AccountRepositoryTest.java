79fc64c872960cbcde5f72c52b92800e7ba14ca8
Namjun Kim <namjunemy@gmail.com>
2019-01-18 13:15:47 +0900

    Spring Data JPA 연동, Repository 테스트 코드 작성(테스트시 H2, 애플리케이션 MySQL)

---
+++ src/test/java/io/namjune/springbootconceptandutilization/account/AccountRepositoryTest.java
@@ -0,0 +1,38 @@
+package io.namjune.springbootconceptandutilization.account;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AccountRepositoryTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void dependencyInjection() throws SQLException {
+        try(Connection connection = dataSource.getConnection()) {
+            DatabaseMetaData metaData = dataSource.getConnection().getMetaData();
+            System.out.println(metaData.getURL());
+            System.out.println(metaData.getDriverName());
+            System.out.println(metaData.getUserName());
+        }
+    }
+}
