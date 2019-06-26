b70fe51b1005e64c1dd78afae6caecff3396c883
Namjun Kim <namjunemy@gmail.com>
2019-06-26 18:48:28 +0900

    Revert "Spring data migration - flyway"
    This reverts commit ed31b16cfdd5f38c18941ada48f6c24907db25f6.

--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists account;
-create table account
-(
-    id       int(10) not null auto_increment,
-    password varchar(255),
-    username varchar(255),
-    primary key (id)
-);
